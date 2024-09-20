1f04b19d6c7a62cc3bf8eed5b4e858c3009545c0
Josan Mihai <mihai.josan12@gmail.com>
2024-09-20 10:32:41 +0300

    Added product preview

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Urbanist } from "next/font/google"
 import type { Metadata } from "next"
 
 import "./globals.css"
+import ModalProvider from "@/providers/modal-provider"
 
 const font = Urbanist({ subsets: ["latin"] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={font.className}>
+                <ModalProvider />
                 <Navbar />
                 {children}
                 <Footer />
